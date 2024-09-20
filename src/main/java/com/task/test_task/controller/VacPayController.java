883507f212562efb597826074c73565c5db4bb4d
Walentin <valession@gmail.com>
2024-09-20 20:12:03 +0300

    Добавлен контроллер для обработки API /calculate

---
+++ src/main/java/com/task/test_task/controller/VacPayController.java
@@ -0,0 +1,8 @@
+package com.task.test_task.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/calculate")
+public class VacPayController {
+}
