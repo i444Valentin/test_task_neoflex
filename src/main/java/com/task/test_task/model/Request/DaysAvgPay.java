cb54ba3c8662f9eb7143ec10d83bb03db38a3bc9
Walentin <valession@gmail.com>
2024-09-21 19:27:10 +0300

    Добавлены модели: DaysAvgPay - количество дней отпуска и средняя зарплата VacSums - сумма отпускных

---
+++ src/main/java/com/task/test_task/model/Request/DaysAvgPay.java
@@ -0,0 +1,9 @@
+package com.task.test_task.model.Request;
+
+import lombok.Data;
+
+@Data
+public class DaysAvgPay {
+    private Integer daysCount;
+    private Double averagePay;
+}
---
+++ src/main/java/com/task/test_task/model/Response/VacSums.java
@@ -0,0 +1,8 @@
+package com.task.test_task.model.Response;
+
+import lombok.Data;
+
+@Data
+public class VacSums {
+    private Double vacPay;
+}
