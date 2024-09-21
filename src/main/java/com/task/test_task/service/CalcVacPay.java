c86426497c0bfb108cbab465b55d1da56725222e
Walentin <valession@gmail.com>
2024-09-21 22:35:49 +0300

    Изменен подсчет суммы отпускных, теперь выводится отпускные, округленные до сотых (копейки)

--- src/main/java/com/task/test_task/service/CalcVacPay.java
+++ src/main/java/com/task/test_task/service/CalcVacPay.java
@@ -2,6 +2,7 @@ package com.task.test_task.service;
 
 import com.task.test_task.model.Request.DaysAvgPay;
 import com.task.test_task.model.Response.VacSums;
+import com.task.test_task.utils.MathUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,9 +10,9 @@ public class CalcVacPay {
 
     public VacSums getVacSums(DaysAvgPay daysAvgPay){
         Double averageDayPay = daysAvgPay.getAveragePay() / 29.3;
-        Double vacSums = averageDayPay * daysAvgPay.getDaysCount();
-
+        double vacSums = averageDayPay * daysAvgPay.getDaysCount();
         VacSums vSums = new VacSums();
+        vacSums = MathUtils.round(vacSums,2);
         vSums.setVacPay(vacSums);
         return vSums;
     }
