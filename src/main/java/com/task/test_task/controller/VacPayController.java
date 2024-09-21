f2fcb2ac953027cc758ca7f73473f835f1340ff8
Walentin <valession@gmail.com>
2024-09-21 22:34:48 +0300

    Добавлен функционал обработки для контроллера

--- src/main/java/com/task/test_task/controller/VacPayController.java
+++ src/main/java/com/task/test_task/controller/VacPayController.java
@@ -1,8 +1,22 @@
 package com.task.test_task.controller;
+import com.task.test_task.model.Request.DaysAvgPay;
+import com.task.test_task.model.Response.VacSums;
+import com.task.test_task.service.CalcVacPay;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/calculate")
 public class VacPayController {
+
+    private final CalcVacPay calcVacPay;
+
+    @Autowired
+    public VacPayController(CalcVacPay calcVacPay){
+        this.calcVacPay = calcVacPay;
+    }
+    @GetMapping
+    public VacSums getVacSums(DaysAvgPay daysAvgPay){
+        return calcVacPay.getVacSums(daysAvgPay);
+    }
 }
